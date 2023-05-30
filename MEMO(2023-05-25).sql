1f9ff55d0bbae15891afb64e566f9c0a3337d619
cccff12 <ksun0430@naver.com>
2023-05-30 17:57:52 +0900

    shoping 시험

--- MEMO(2023-05-25).sql
+++ MEMO(2023-05-25).sql
@@ -15,16 +15,14 @@ m_content	nVARCHAR2(10)
 );
 
 insert into tbl_memo( m_seq, m_writer, m_date,m_subject,m_content)
-VALUES('1111111111','홍길동','20230525','제목','내용');
+VALUES('1','홍길동','20230525','제목','내용');
 
 insert into tbl_memo( m_seq, m_writer, m_date,m_subject,m_content)
-VALUES('2222222222','홍길동','20230525','제목','내용');
+VALUES('2','성춘향','20230525','제목','내용');
 
 insert into tbl_memo( m_seq, m_writer, m_date,m_subject,m_content)
-VALUES('3333333333','홍길동','20230525','제목','내용');
+VALUES('3','이몽룡','20230525','제목','내용');
 
-insert into tbl_memo( m_seq, m_writer, m_date,m_subject,m_content)
-VALUES('4444444444','홍길동','20230525','제목','내용');
 
 select * from tbl_memo;
 
---
+++ shopingcart(2023-05-30).sql
@@ -0,0 +1,38 @@
+--DROP TABLE tbl_;
+
+CREATE TABLE tbl_buyer(
+
+buId	VARCHAR2(10)		PRIMARY KEY,
+buName	nVARCHAR2(20),		
+buTel	VARCHAR2(10),		
+buAddr	nVARCHAR2(125)		
+);
+
+create table tbl_product(
+pCode	VARCHAR2(13)		PRIMARY KEY,
+pName	nVARCHAR2(30),		
+pItem	VARCHAR2(10),		
+pIPrice	NUMBER,		
+pOPrice	NUMBER	
+);
+
+
+create table tbl_iolist(
+ioSEQ	NUMBER		PRIMARY KEY,
+ioDate	VARCHAR2(10),		
+ioTime	VARCHAR2(10),		
+ioBuId	VARCHAR2(10),		
+ioPCode	NUMBER,		
+ioQuan	NUMBER,		
+ioPrice	NUMBER		
+);
+
+INSERT INTO tbl_buyer(buId, buName, buTel, buAddr)
+values('1','홍길동','0101111111','서울');
+
+
+INSERT INTO tbl_buyer(buId, buName, buTel, buAddr)
+values('2','성춘향','0102222222','서울');
+
+
+
